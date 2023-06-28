7ae40a126cb910d40fe7045b25ca08e801c05ff8
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-28 00:17:13 +0000

    oyo3 7wari

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,15 +6,15 @@ class BookCommentsController < ApplicationController
     comment.save
     redirect_to book_path(book)
   end
-  
+
   def destroy
-    
+
     BookComment.find(params[:id]).destroy
     redirect_to book_path(params[:book_id])
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
